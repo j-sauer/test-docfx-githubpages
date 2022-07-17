e42bcf485cc3967e4950aea8040a56d71cf8511e
Jens Sauer <jens.sauer@conweaver.com>
2022-07-18 00:10:10 +0200

    Intial

---
+++ src/TestLib/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace TestLib;
+/// <summary>
+/// Summary of my Class1
+/// </summary>
+public class Class1
+{
+    /// <summary>
+    /// Summary of Prop1
+    /// </summary>
+    public string Prop1 { get; set; }
+}
